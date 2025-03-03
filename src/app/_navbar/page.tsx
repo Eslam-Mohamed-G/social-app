ec6c22236d8e0b5e099111bfbb39baa6ef9e1d42
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-03-03 17:53:14 +0200

    add formik and yup to handle signup

--- src/app/_navbar/page.tsx
+++ src/app/_navbar/page.tsx
@@ -41,7 +41,6 @@ function Navbar() {
         <AppBar position="static">
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
-                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
                     <Typography
                         variant="h6"
                         noWrap
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      
+      <h1>home pages</h1>
     </div>
   );
 }
