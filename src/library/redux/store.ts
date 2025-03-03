645cac6e7798462c92d4e9d188822bf5a14a21a6
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-03-03 23:26:35 +0200

    handle store.ts

--- src/library/redux/store.ts
+++ src/library/redux/store.ts
@@ -1,7 +1,12 @@
+import userReducer from "./useSlice";
+
 const { configureStore } = require("@reduxjs/toolkit");
 
 export const store = configureStore ({
     reducer:{
-
+        user: userReducer
     }
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
