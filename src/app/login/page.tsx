a9e92741aa853ac8818c946a9540cb0d88880e45
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-03-05 17:17:10 +0200

    handle login page desgin

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,40 @@
+import { Box, Button, Container, Paper, TextField, Typography } from '@mui/material';
 import React from 'react';
 
 export default function Login() {
     return (
-        <div>Login</div>
+        <Container maxWidth="md" className='login-container'>
+            <Paper elevation={8} sx={{ margin: "auto", textAlign:"end" }}>
+                <Typography
+                    sx={{
+                        color: '#1976d2',
+                        textAlign: "center",
+                        fontSize: { xs: "24px", md: "34px" },
+                        paddingTop: "20px",
+                    }}>Login Now
+                </Typography>
+                <Box
+                    component="form"
+                    sx={{ padding: '20px', paddingTop:"0px" }}
+                    noValidate
+                >
+                    <TextField
+                        id="email"
+                        name='email'
+                        label="Email"
+                        variant="outlined"
+                        sx={{ width: '100%', marginTop: '15px' }}
+                    />
+                    <TextField
+                        id="password"
+                        name='password'
+                        label="Password"
+                        variant="outlined"
+                        sx={{ width: '100%', marginTop: '15px' }}
+                    />
+                    <Button type='submit' variant="outlined" sx={{ marginTop: "15px" }}>login</Button>
+                </Box>
+            </Paper>
+        </Container>
     )
 };
