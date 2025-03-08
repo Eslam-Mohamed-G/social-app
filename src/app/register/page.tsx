615864f5924b5800df5897d4535c8560a70cd457
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-03-08 17:15:02 +0200

    handle register page desgin

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,8 @@
 "use client";
-import { Box, Container, Paper, TextField } from '@mui/material';
-import React from 'react';
+import { Box, Button, Container, FormControl, IconButton, InputAdornment, InputLabel, MenuItem, OutlinedInput, Paper, Select, TextField, Typography } from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import * as React from 'react';
 
 interface SignupValues {
   name: string;
@@ -12,6 +14,17 @@ interface SignupValues {
 }
 
 export default function Register() {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  const handleMouseUpPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
   const initialValues: SignupValues = {
     name: '',
     email: '',
@@ -21,54 +34,112 @@ export default function Register() {
     gender: '',
   }
   return (
-    <Container maxWidth='md'>
-      <Paper sx={{ width: '100%', margin: 'auto' }} elevation={24}>
+    <Container maxWidth='md' sx={{ padding: "20px" }}>
+      <Paper sx={{ width: '100%', margin: 'auto', textAlign: "end" }} elevation={8}>
+        <Typography
+          sx={{
+            color: '#1976d2',
+            textAlign: "center",
+            fontSize: { xs: "24px", md: "34px" },
+            paddingTop: "20px",
+          }}>Register
+        </Typography>
         <Box
           component="form"
-          sx={{ marginBlock: '20px', padding:'20px' }}
+          sx={{ padding: '20px' }}
         >
           <TextField
             id="name"
+            type='text'
             name='name'
             label="Name"
             variant="outlined"
-            sx={{ width: '100%', marginTop:'15px' }}
+            sx={{ width: '100%', marginTop: '15px' }}
           />
           <TextField
             id="email"
             name='email'
+            type='email'
             label="Email"
             variant="outlined"
-            sx={{ width: '100%', marginTop:'15px' }}
+            sx={{ width: '100%', marginTop: '15px' }}
           />
-          <TextField
-            id="password"
-            name='password'
-            label="Password"
-            variant="outlined"
-            sx={{ width: '100%', marginTop:'15px' }}
-          />
-          <TextField
-            id="rePassword"
-            name='rePassword'
-            label="rePassword"
-            variant="outlined"
-            sx={{ width: '100%', marginTop:'15px' }}
-          />
+          <FormControl sx={{ width: '100%', marginTop: '15px' }} variant="outlined">
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <OutlinedInput
+              id="password"
+              name='password'
+              type={showPassword ? 'text' : 'password'}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label={
+                      showPassword ? 'hide the password' : 'display the password'
+                    }
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    onMouseUp={handleMouseUpPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+
+          <FormControl sx={{ width: '100%', marginTop: '15px' }} variant="outlined">
+            <InputLabel htmlFor="repassword">RePassword</InputLabel>
+            <OutlinedInput
+              id="repassword"
+              name='repassword'
+              type={showPassword ? 'text' : 'password'}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label={
+                      showPassword ? 'hide the password' : 'display the password'
+                    }
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    onMouseUp={handleMouseUpPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="RePassword"
+            />
+          </FormControl>
+
           <TextField
             id="dateOfBirth"
             name='dateOfBirth'
+            type='date'
+            slotProps={{
+              inputLabel: {
+                shrink: true,
+              },
+            }}
             label="Birth Day"
             variant="outlined"
-            sx={{ width: '100%', marginTop:'15px' }}
+            sx={{ width: '100%', marginTop: '15px' }}
           />
-          <TextField
-            id="gender"
-            name='gender'
-            label="gender"
-            variant="outlined"
-            sx={{ width: '100%', marginTop:'15px' }}
-          />
+          <FormControl sx={{ width: '100%', marginTop: '15px' }}>
+            <InputLabel id="demo-simple-select-label">Gender</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              name='gender'
+              label="Gender"
+            >
+              <MenuItem value={"male"}>Male</MenuItem>
+              <MenuItem value={"female"}>Female</MenuItem>
+            </Select>
+          </FormControl>
+          <Button type='submit' variant="outlined" sx={{ marginTop: "15px" }}>Register</Button>
         </Box>
       </Paper>
     </Container>
