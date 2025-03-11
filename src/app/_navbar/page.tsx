e3491f3d2c43ad713c210587114ca386abeec244
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-03-11 13:52:42 +0200

    Display navbar content based on whether it is loggedin or not

--- src/app/_navbar/page.tsx
+++ src/app/_navbar/page.tsx
@@ -66,16 +66,18 @@ function Navbar() {
                     </Typography>
 
                     <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
+                        {isClient && isLoggedIn &&
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            >
+                                <MenuIcon />
+                            </IconButton>}
+
                         <Menu
                             id="menu-appbar"
                             anchorEl={anchorElNav}
