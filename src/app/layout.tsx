389408296826fb890975aee64f928e13cb40214b
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-03-12 18:47:44 +0200

    Added NAvbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 "use client";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./_navbar/page";
+// import Navbar from "./_navbar/page";
 import { ThemeProvider } from '@mui/material/styles';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import theme from "./theme";
 import { Provider } from "react-redux";
 import { store } from "../library/redux/store";
 import { Toaster } from 'react-hot-toast';
+import Navbar from "./_components/Navbar";
 
 
 const geistSans = Geist({
