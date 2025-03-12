852ca1d49edcb17f5f00fceb5332087047b482b6
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-03-12 14:25:55 +0200

    Added post component

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,8 +1,10 @@
 "use client"
 import { PostsI } from '@/interfaces/Posts';
 import { getAllPosts } from '@/library/redux/postsSlice';
+import { Container, Stack, Typography } from '@mui/material';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Post from '../_components/Post';
 
 export default function Posts() {
     const dispatch = useDispatch<any>()
@@ -13,8 +15,13 @@ export default function Posts() {
         dispatch(getAllPosts()) 
     }, [])
     return (
-        <div>
-            postes
-        </div>
+        <Container maxWidth="sm">
+            <Typography variant='h5' color='initial'>Posts</Typography>
+            <Stack spacing={3} marginTop={3}>
+                {posts.map((post)=>(
+                    <Post post={post} key={post._id}/>
+                ))}
+            </Stack>
+        </Container>
     )
 };
